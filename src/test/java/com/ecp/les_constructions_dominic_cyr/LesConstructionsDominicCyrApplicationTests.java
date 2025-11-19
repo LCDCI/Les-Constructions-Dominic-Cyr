bb7b364988730f309bc73246325fd3415b7ac9fa
OlivierD <olivedufort@gmail.com>
2025-11-19 14:17:52 -0500

    Basic Setup

---
+++ src/test/java/com/ecp/les_constructions_dominic_cyr/LesConstructionsDominicCyrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecp.les_constructions_dominic_cyr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LesConstructionsDominicCyrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
