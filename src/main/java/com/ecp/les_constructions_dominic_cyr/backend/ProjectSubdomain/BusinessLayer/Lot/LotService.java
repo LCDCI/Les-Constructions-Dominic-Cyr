e288969c3b7f8cfe6b223fa68aa5788a8a6e3569
OlivieriD <olivedufort@gmail.com>
2025-12-04 13:35:57 -0500

    changed file structure

---
+++ src/main/java/com/ecp/les_constructions_dominic_cyr/backend/ProjectSubdomain/BusinessLayer/Lot/LotService.java
@@ -0,0 +1,14 @@
+package com.ecp.les_constructions_dominic_cyr.backend.ProjectSubdomain.BusinessLayer.Lot;
+
+import com.ecp.les_constructions_dominic_cyr.backend.ProjectSubdomain.PresentationLayer.Lot.LotRequestModel;
+import com.ecp.les_constructions_dominic_cyr.backend.ProjectSubdomain.PresentationLayer.Lot.LotResponseModel;
+
+import java.util.List;
+
+public interface LotService {
+    List<LotResponseModel> getAllLots();
+    LotResponseModel getLotById(String lotId);
+    LotResponseModel addLot(LotRequestModel lotRequestModel);
+    LotResponseModel updateLot(LotRequestModel lotRequestModel, String lotId);
+    void deleteLot(String lotId);
+}
