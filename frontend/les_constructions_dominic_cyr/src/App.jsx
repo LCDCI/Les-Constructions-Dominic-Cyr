0dad1828f27343129671a0f7888b5d809deb13d5
Gazou16 <146896335+Gazou16@users.noreply.github.com>
2025-12-05 18:42:58 -0500

    Updated navbar and footer

--- frontend/les_constructions_dominic_cyr/src/App.jsx
+++ frontend/les_constructions_dominic_cyr/src/App.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AppNavBar from './layouts/AppNavBar';
+import AppFooter from './layouts/AppFooter';
 import Home from './pages/Home';
 import LotsPage from './pages/LotsPage';
 import ServerError from './pages/ServerError';
 import NotFound from './pages/NotFound';
 import NavigationSetter from './components/NavigationSetter';
+import './App.css';
 
 export default function App() {
   return (
     <BrowserRouter>
       <NavigationSetter />
-      <AppNavBar />
-      <main style={{ padding: '16px' }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/lots" element={<LotsPage />} />
-          <Route path="/error" element={<ServerError />} />
-          <Route path="*" element={<NotFound />} />
-          {/* Remember to add more routes here as App grows */}
-        </Routes>
-      </main>
+      <div className="app-container">
+        <AppNavBar />
+        <main style={{ padding: '16px' }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/lots" element={<LotsPage />} />
+            <Route path="/error" element={<ServerError />} />
+            <Route path="*" element={<NotFound />} />
+            {/* Remember to add more routes here as App grows */}
+          </Routes>
+        </main>
+        <AppFooter />
+      </div>
     </BrowserRouter>
   );
 }
