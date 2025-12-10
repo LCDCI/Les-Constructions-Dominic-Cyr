0de1e31723c1ca79ba49a5b4c220bf57f4ffcba7
OlivieriD <olivedufort@gmail.com>
2025-12-10 13:01:13 -0500

    fixed schedule controller, + env

--- src/main/java/com/ecp/les_constructions_dominic_cyr/backend/ProjectSubdomain/PresentationLayer/Schedule/ScheduleController.java
+++ src/main/java/com/ecp/les_constructions_dominic_cyr/backend/ProjectSubdomain/PresentationLayer/Schedule/ScheduleController.java
@@ -1,8 +1,12 @@
 package com.ecp.les_constructions_dominic_cyr.backend.ProjectSubdomain.PresentationLayer.Schedule;
 
 import com.ecp.les_constructions_dominic_cyr.backend.ProjectSubdomain.BusinessLayer.Schedule.ScheduleService;
+import com.ecp.les_constructions_dominic_cyr.backend.utils.Exception.BadRequestException;
+import com.ecp.les_constructions_dominic_cyr.backend.utils.Exception.InvalidRequestException;
+import com.ecp.les_constructions_dominic_cyr.backend.utils.Exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,64 +23,98 @@ public class ScheduleController {
 
     @GetMapping("/owners/schedules")
     public ResponseEntity<List<ScheduleResponseDTO>> getOwnerCurrentWeekSchedules() {
-        log.info("REST request to get current week schedules");
         List<ScheduleResponseDTO> schedules = scheduleService.getCurrentWeekSchedules();
         return ResponseEntity.ok(schedules);
     }
 
     @GetMapping("/owners/schedules/all")
     public ResponseEntity<List<ScheduleResponseDTO>> getOwnerAllSchedules() {
-        log.info("REST request to get all schedules");
         List<ScheduleResponseDTO> schedules = scheduleService.getAllSchedules();
         return ResponseEntity.ok(schedules);
     }
 
     @GetMapping("/owners/schedules/{scheduleIdentifier}")
     public ResponseEntity<ScheduleResponseDTO> getOwnerScheduleByIdentifier(@PathVariable String scheduleIdentifier) {
-        log.info("REST request to get schedule by identifier: {}", scheduleIdentifier);
-        ScheduleResponseDTO schedule = scheduleService.getScheduleByIdentifier(scheduleIdentifier);
-        return ResponseEntity.ok(schedule);
+        try {
+            ScheduleResponseDTO schedule = scheduleService.getScheduleByIdentifier(scheduleIdentifier);
+            return ResponseEntity.ok(schedule);
+        } catch (NotFoundException ex) {
+            return new ResponseEntity(ex.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (BadRequestException | InvalidRequestException ex) {
+            return new ResponseEntity(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/salesperson/schedules")
     public ResponseEntity<List<ScheduleResponseDTO>> getSalespersonCurrentWeekSchedules() {
-        log.info("REST request to get current week schedules");
         List<ScheduleResponseDTO> schedules = scheduleService.getCurrentWeekSchedules();
         return ResponseEntity.ok(schedules);
     }
 
     @GetMapping("/salesperson/schedules/all")
     public ResponseEntity<List<ScheduleResponseDTO>> getSalespersonAllSchedules() {
-        log.info("REST request to get all schedules");
         List<ScheduleResponseDTO> schedules = scheduleService.getAllSchedules();
         return ResponseEntity.ok(schedules);
     }
 
     @GetMapping("/salesperson/schedules/{scheduleIdentifier}")
     public ResponseEntity<ScheduleResponseDTO> getSalespersonScheduleByIdentifier(@PathVariable String scheduleIdentifier) {
-        log.info("REST request to get schedule by identifier: {}", scheduleIdentifier);
-        ScheduleResponseDTO schedule = scheduleService.getScheduleByIdentifier(scheduleIdentifier);
-        return ResponseEntity.ok(schedule);
+        try {
+            ScheduleResponseDTO schedule = scheduleService.getScheduleByIdentifier(scheduleIdentifier);
+            return ResponseEntity.ok(schedule);
+        } catch (NotFoundException ex) {
+            return new ResponseEntity(ex.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (BadRequestException | InvalidRequestException ex) {
+            return new ResponseEntity(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/contractors/schedules")
     public ResponseEntity<List<ScheduleResponseDTO>> getContractorCurrentWeekSchedules() {
-        log.info("REST request to get current week schedules");
         List<ScheduleResponseDTO> schedules = scheduleService.getCurrentWeekSchedules();
         return ResponseEntity.ok(schedules);
     }
 
     @GetMapping("/contractors/schedules/all")
     public ResponseEntity<List<ScheduleResponseDTO>> getContractorAllSchedules() {
-        log.info("REST request to get all schedules");
         List<ScheduleResponseDTO> schedules = scheduleService.getAllSchedules();
         return ResponseEntity.ok(schedules);
     }
 
     @GetMapping("/contractors/schedules/{scheduleIdentifier}")
     public ResponseEntity<ScheduleResponseDTO> getContractorScheduleByIdentifier(@PathVariable String scheduleIdentifier) {
-        log.info("REST request to get schedule by identifier: {}", scheduleIdentifier);
-        ScheduleResponseDTO schedule = scheduleService.getScheduleByIdentifier(scheduleIdentifier);
-        return ResponseEntity.ok(schedule);
+        try {
+            ScheduleResponseDTO schedule = scheduleService.getScheduleByIdentifier(scheduleIdentifier);
+            return ResponseEntity.ok(schedule);
+        } catch (NotFoundException ex) {
+            return new ResponseEntity(ex.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (BadRequestException | InvalidRequestException ex) {
+            return new ResponseEntity(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
+
+    @GetMapping("/customers/schedules")
+    public ResponseEntity<List<ScheduleResponseDTO>> getCustomerCurrentWeekSchedules() {
+        List<ScheduleResponseDTO> schedules = scheduleService.getCurrentWeekSchedules();
+        return ResponseEntity.ok(schedules);
+    }
+
+    @GetMapping("/customers/schedules/all")
+    public ResponseEntity<List<ScheduleResponseDTO>> getCustomerAllSchedules() {
+        List<ScheduleResponseDTO> schedules = scheduleService.getAllSchedules();
+        return ResponseEntity.ok(schedules);
+    }
+
+    @GetMapping("/customers/schedules/{scheduleIdentifier}")
+    public ResponseEntity<ScheduleResponseDTO> getCustomerScheduleByIdentifier(@PathVariable String scheduleIdentifier) {
+        try {
+            ScheduleResponseDTO schedule = scheduleService.getScheduleByIdentifier(scheduleIdentifier);
+            return ResponseEntity.ok(schedule);
+        } catch (NotFoundException ex) {
+            return new ResponseEntity(ex.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (BadRequestException | InvalidRequestException ex) {
+            return new ResponseEntity(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
 }
