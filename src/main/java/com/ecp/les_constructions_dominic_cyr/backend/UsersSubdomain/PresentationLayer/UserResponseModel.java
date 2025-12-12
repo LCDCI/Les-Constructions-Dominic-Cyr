3b3a098117996cea41fbd80818aa926c02f363d7
ericoder06 <eric.kossovsky@gmail.com>
2025-12-12 10:59:47 -0500

    The backend has been completed (Initialised user creation).

---
+++ src/main/java/com/ecp/les_constructions_dominic_cyr/backend/UsersSubdomain/PresentationLayer/UserResponseModel.java
@@ -0,0 +1,88 @@
+package com.ecp.les_constructions_dominic_cyr.backend.UsersSubdomain.PresentationLayer;
+
+import com.ecp.les_constructions_dominic_cyr.backend.UsersSubdomain.DataAccessLayer.UserRole;
+
+public class UserResponseModel {
+
+    private String userIdentifier; // String representation of the UUID
+    private String firstName;
+    private String lastName;
+
+    // Primary login email
+    private String primaryEmail;
+
+    // Secondary contact email
+    private String secondaryEmail;
+
+    private String phone;
+    private UserRole userRole;
+
+    private String inviteLink;
+
+    public UserResponseModel() {
+    }
+
+    public String getUserIdentifier() {
+        return userIdentifier;
+    }
+
+    public void setUserIdentifier(String userIdentifier) {
+        this.userIdentifier = userIdentifier;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPrimaryEmail() {
+        return primaryEmail;
+    }
+
+    public void setPrimaryEmail(String primaryEmail) {
+        this.primaryEmail = primaryEmail;
+    }
+
+    public String getSecondaryEmail() {
+        return secondaryEmail;
+    }
+
+    public void setSecondaryEmail(String secondaryEmail) {
+        this.secondaryEmail = secondaryEmail;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(UserRole userRole) {
+        this.userRole = userRole;
+    }
+
+    public String getInviteLink() {
+        return inviteLink;
+    }
+
+    public void setInviteLink(String inviteLink) {
+        this.inviteLink = inviteLink;
+    }
+}
