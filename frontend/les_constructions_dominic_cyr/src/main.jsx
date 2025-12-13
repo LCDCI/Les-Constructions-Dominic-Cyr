15295c71f91c6966fdfe519993f0123ce847bced
ericoder06 <eric.kossovsky@gmail.com>
2025-12-13 11:21:31 -0500

    Implemented Login (Without SignIn allowed). Missing tests

--- frontend/les_constructions_dominic_cyr/src/main.jsx
+++ frontend/les_constructions_dominic_cyr/src/main.jsx
@@ -1,7 +1,10 @@
+// src/main.jsx
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
+import './utils/i18n'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Auth0Provider
