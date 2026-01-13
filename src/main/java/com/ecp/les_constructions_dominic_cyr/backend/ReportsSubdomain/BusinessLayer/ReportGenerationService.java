c8860587751351ac64abe45b08c1779cf18d010c
OlivieriD <olivedufort@gmail.com>
2026-01-12 22:37:41 -0500

    Revert "initial backend complete"
    This reverts commit bf4118e1711a162dd8f847db8b6d4a4cc294710a.

--- src/main/java/com/ecp/les_constructions_dominic_cyr/backend/ReportsSubdomain/BusinessLayer/ReportGenerationService.java
+++
@@ -1,7 +0,0 @@
-package com.ecp.les_constructions_dominic_cyr.backend.ReportsSubdomain.BusinessLayer;
-
-import com.ecp.les_constructions_dominic_cyr. backend.ReportsSubdomain. DataAccessLayer.AnalyticsReport;
-
-public interface ReportGenerationService {
-    byte[] generateReportFile(AnalyticsReport report) throws Exception;
-}
