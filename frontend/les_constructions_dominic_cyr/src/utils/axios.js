d7e04e0fbe8668c5b6709cba739d365af6ef898a
ericoder06 <eric.kossovsky@gmail.com>
2026-01-20 17:12:29 -0500

    Fixed Security Issues

--- frontend/les_constructions_dominic_cyr/src/utils/axios.js
+++ frontend/les_constructions_dominic_cyr/src/utils/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_APP_API_URL || 'http://localhost:8080/api/v1',
+  baseURL: import.meta.env.VITE_APP_API_URL || '/api/v1',
   timeout: 10000,
   withCredentials: true,
   headers: {
