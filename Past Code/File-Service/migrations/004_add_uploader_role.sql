0989cc016f0a90612c1e45a285166de15eed217f
ericoder06 <eric.kossovsky@gmail.com>
2026-01-27 16:52:47 -0500

    Removed Past Code (Useless Folder)

--- Past Code/File-Service/migrations/004_add_uploader_role.sql
+++
@@ -1,5 +0,0 @@
--- Add uploader_role column to files table for role-based filtering
-ALTER TABLE files ADD COLUMN IF NOT EXISTS uploader_role VARCHAR(20);
-
--- Add index for better query performance
-CREATE INDEX IF NOT EXISTS idx_files_uploader_role ON files(uploader_role);
