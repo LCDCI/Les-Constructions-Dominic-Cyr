66621a06cd8e4036f79132dc22100e98f6f161d8
josevmorilla <super.jose.bros@icloud.com>
2026-02-02 09:29:30 -0500

    Fix capitalization in footer labels for consistency

--- frontend/les_constructions_dominic_cyr/src/components/Footers/HomeFooter.jsx
+++ frontend/les_constructions_dominic_cyr/src/components/Footers/HomeFooter.jsx
@@ -55,7 +55,7 @@ export default function HomeFooter() {
           </div>
           <div className="footer-content">
             <p className="footer-item">
-              <strong>{t('footer.rbqLabel', 'Régie du bâtiment du Québec')}:</strong> {t('footer.rbq', '8356-0169-03')}
+              <strong>{t('footer.rbqLabel', 'Régie du Bâtiment du Québec')}:</strong> {t('footer.rbq', '8356-0169-03')}
             </p>
             <a
               href="https://www.pes.rbq.gouv.qc.ca/RegistreLicences/Recherche/Resultats/FicheDetenteur?mode=Entreprise&crit=r1TSc6INGEWKtcUK6jcX9%2FEByYB22EJQyiTukjrpiNiBdLFBdmxsUQD5hDhRnpniSWRgQPOq4Oq7AYd4UqvduXNoxS7GrtZarAE5%2FQv2Tj9k9EtXsKZODcycaRS2vCDNPQ6XfEMFITtraZrvf%2BgSWYCujnyk5pjSv%2Bn4i4fIsPKcf%2BPb8Z5RSbvKvHajTbuyX5Dvx9GsimL%2FOHlp6DxDoJ5FP8q94u2ewsoBsQq0VhthO9VtsuE1wYESAcFhyiUIbnNiMS6dBHk2XPVhhCVXDs93MtpN4cuBS%2B3XvsKTj7ZIHNuYwyiYsaYFfpuOofJulS3vTsVihLb7898tOi0SsOtAnfY2%2BFYerzER6TO4TIM7Fwh25dHZC5AARA6k0Q1JFPckDujovDNBflrWPX6ipKRRisQTIjkNwowf0MZXpEs%2F9t2hOEjx29qd%2Fp%2FgVVXh0pnV%2BAcmHQdRVcT0Ib9L2OKI53F4OEdOzktVpYlvF5e%2Byd4pInDRrybDEWGQd2tXuL8EwlgAwkwSYbbVXhT%2BGvjvvD%2B%2BOAeNLHpvnyGWvd%2BqyeTv%2Bxap7%2FwaThYJmcMvtrVpO3LhkjxCVdyyhvZ4rR6L%2BGVoit76HJclXqBxW4HyXwZOY7DMXlaBZFEJt2JcrKLEn5aHNyjOPYVKcq9qorx7obYiAyJ57dVpEs8BqXg645HMfZqPMSmTQPiCBoHL&ent=Dfp5TGXsmaSEEtcnF555TQ%3D%3D"
@@ -67,7 +67,7 @@ export default function HomeFooter() {
             </a>
 
             <p className="footer-item" style={{ marginTop: '8px' }}>
-              <strong>{t('footer.gcrLabel', 'Garantie construction résidentielle')}:</strong> {t('footer.gcr', '11084')}
+              <strong>{t('footer.gcrLabel', 'Garantie Construction Résidentielle')}:</strong> {t('footer.gcr', '11084')}
             </p>
             <a
               href="https://repertoire.garantiegcr.com/entrepreneurs/8356-0169-03.html
